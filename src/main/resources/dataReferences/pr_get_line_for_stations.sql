9687091d926fe062e76d2e8c416463ad25ab9339
Przemo <Przemo@Przemo-Komputer>
2015-08-15 14:47:16 +0100

    stored procedure modified

--- src/main/resources/dataReferences/pr_get_line_for_stations.sql
+++ src/main/resources/dataReferences/pr_get_line_for_stations.sql
@@ -2,8 +2,8 @@
 CREATE OR REPLACE FUNCTION pr_get_line_for_stations(
     station_from integer,
     station_to integer)
-  RETURNS integer AS
-$BODY$select distinct l.* from lines l inner join timetables t on l.id=t.id_lines where (select count(id) from timetables where node_from=$1
+  RETURNS lines AS
+$BODY$select distinct l.id as id, l.id_bus as bus, l.decription as description from lines l inner join timetables t on l.id=t.id_lines where (select count(id) from timetables where node_from=$1
 and id_lines=t.id_lines)>0 and (select count(id) from timetables where node_to=$2
 and id_lines = t.id_lines)>0$BODY$
   LANGUAGE sql VOLATILE
